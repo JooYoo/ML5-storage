f3d3b2c694f955e9a805be9ea1dc01f5d00cf0f8
JooYooGit <jimmy.zhuyu@gmail.com>
2019-02-12 04:40:20 +0100

    created project

---
+++ js/train.js
@@ -0,0 +1,104 @@
+var video = document.getElementById('video');
+var videoStatus = document.getElementById('videoStatus');
+var loading = document.getElementById('loading');
+var aBtn = document.getElementById('a-btn');
+var bBtn = document.getElementById('b-btn');
+var aCount = document.getElementById('sampleA-count');
+var bCount = document.getElementById('sampleB-count');
+var containerA = document.getElementById('canvas-container-a');
+var containerB = document.getElementById('canvas-container-b');
+var train = document.getElementById('train-btn');
+var loss = document.getElementById('loss');
+var result = document.getElementById('result');
+var predict = document.getElementById('predict');
+
+let totalloss = 0;
+
+// get stream from cam
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+// extract features from MobileNet
+const featureExtractor = ml5.featureExtractor('MobileNet', function () {
+    loading.innerText = "Model loaded!";
+    // create classifier
+    const classifier = featureExtractor.classification(video, function () {
+        videoStatus.innerText = 'Video ready!'
+    });
+
+    // add SampleA
+    aBtn.onclick = function () {
+        classifier.addImage('SampleA');
+        console.log('add SampleA');
+    }
+    // add SampleB
+    bBtn.onclick = function () {
+        classifier.addImage('SampleB');
+        console.log('add SampleB');
+    }
+
+    // train
+    train.onclick = function () {
+        classifier.train(function (lossValue) {
+            if (lossValue) {
+                totalloss = lossValue;
+                loss.innerHTML = "loss: " + totalloss;
+            }
+        });
+    }
+
+    // get result
+    predict.onclick = function () {
+        classifier.classify(gotResults);
+    }
+    function gotResults(err, data) {
+        if (err) {
+            console.error(err);
+        }
+        result.innerText = data;
+        classifier.classify(gotResults);
+    }
+});
+
+
+// Sample A: takePhoto
+function takeScreenshotA() {
+    console.log('mouseDown');
+    let canvas = document.createElement('canvas');
+    canvas.width = video.offsetWidth;
+    canvas.height = video.offsetHeight;
+
+    let canvasContext = canvas.getContext("2d");
+    canvasContext.drawImage(
+        video,
+        0, 0,
+        video.offsetWidth, video.offsetHeight
+    );
+
+    containerA.prepend(canvas);
+
+    aCount.innerText = Number(aCount.innerText) + 1;
+}
+
+// Sample B: takePhoto
+function takeScreenshotB() {
+    console.log('mouseDown');
+    let canvas = document.createElement('canvas');
+    canvas.width = video.offsetWidth;
+    canvas.height = video.offsetHeight;
+
+    let canvasContext = canvas.getContext("2d");
+    canvasContext.drawImage(
+        video,
+        0, 0,
+        video.offsetWidth, video.offsetHeight
+    );
+
+    containerB.prepend(canvas);
+
+    bCount.innerText = Number(bCount.innerText) + 1;
+}
